9b874f50c94a6e75365bdcabf97798f5fbf486c7
CESAR TZOC <csartalva01@gmail.com>
2021-04-11 23:15:11 -0600

    Se agrego wison.js

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+//var Parser = require("../../assets/js/wison.js");
+declare var wison;
 
 @Component({
 	selector: 'app-editor',
@@ -14,6 +16,13 @@ export class EditorComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit(): void {
+		//console.log(Parser);
 	}
 
+	readText() {
+		if(this.text != "") {
+			console.log(this.text);
+			wison.parse(this.text);
+		}
+	}
 }
